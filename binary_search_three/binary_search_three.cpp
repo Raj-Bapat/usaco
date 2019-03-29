7414b50face2c9c9a5eb5b626711a64046a90c11
Raj Bapat <a2g2gonzales@gmail.com>
2019-03-28 22:01:54 -0700

    lol

--- binary_search_three/binary_search_three.cpp
+++ binary_search_three/binary_search_three.cpp
@@ -18,7 +18,28 @@ using namespace std;
 
 int N;
 vector<int> bst[100010];
+int depth[100010];
+set<int> existing;
 
 int main() {
+    cin >> N;
+    for (int i = 0; i<N; i++) {
+        int a;
+        cin >> a;
+        if (existing.find(a) != existing.end()) {
+            continue;
+        }
+        auto it = existing.lower_bound(a);
+        if (it == existing.end()) {
+            bst[*(--it)].push_back(a);
+            depth[a] = depth[*(it)]+1;
+            existing.insert(a);
+        } else if (it == existing.begin()) {
+            bst[*(it)].push_back(a);
+            depth[a] = depth[*(it)]+1;
+            existing.insert(a);
+        } else {
 
+        }
+    }
 }
